3bee5b0f7a8322c3e12ef45260d5c0fc97e71d92
Yasu112 <plum168world@gmail.com>
2022-08-14 01:42:03 +0900

    ブログ作成機能の実装

---
+++ blog/resources/views/posts/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link 
+        href="https:/
+        /fonts.googleapis.com/css?family=Nunito:200,600"
+        rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+            
+            
+        </div>
+        
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+        
+        <h2 class='title'>
+            @foreach ($posts as $post)
+                
+                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                
+            @endforeach
+            
+        </h2>
+        
+        [<a href='/posts/create'>create</a>]
+        
+    </body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -10,9 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::get('/posts', 'PostController@index');
+
+Route::post('/posts', 'PostController@store');
+
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/create', 'PostController@create');
+
+Route::get('/posts/{post}', 'PostController@show');
+
+
